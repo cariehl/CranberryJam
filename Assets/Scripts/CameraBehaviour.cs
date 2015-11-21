1425465ce5eb8a4868a471bdfa4b4e61b18807b8
Cooper Riehl <cariehl@umich.edu>
2015-11-21 15:15:56 -0500

    Updated camera behavior to use player's BoxCollider instead of Sprite

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,20 +4,23 @@ using System.Collections;
 public class CameraBehaviour : MonoBehaviour {
 
     public GameObject player;
-    public float upperBound = 0.8f;
+    public float upperBound;
+
+	BoxCollider2D playerCollider;
 	float playerTopY;
 	float playerHeight;
 
     // Use this for initialization
     void Start ()
 	{
-		playerHeight = player.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+		playerCollider = player.GetComponent<BoxCollider2D>();
+		playerHeight = playerCollider.bounds.size.y;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		playerTopY = player.transform.position.y + playerHeight;
+		playerTopY = playerCollider.bounds.min.y + playerHeight;
 		float camHeightY = Camera.main.ViewportToWorldPoint(new Vector3(0, upperBound, Camera.main.nearClipPlane)).y;
         if (playerTopY > camHeightY) {
 			transform.position += new Vector3(0, playerTopY - camHeightY, 0);
