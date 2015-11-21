a63e631ce1e53509371f543d49b3ce27c3d59ff9
Cooper Riehl <cariehl@umich.edu>
2015-11-21 13:42:10 -0500

    Fixed platform segments being tagged incorrectly

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+	// Target jump height in terms of units
+	public float targetJumpHeight = 3.2f;
 
-    public float speed = 1;
-    public float jumpforce = 69;
-    private Rigidbody2D rgbd;
+	float jumpForce;
+    Rigidbody2D rgbd;
     bool grounded = false;
     Animator anim;
 
 	// Use this for initialization
-	void Start () {
-        rgbd = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+	void Start ()
+	{
+		jumpForce = Mathf.Sqrt(2f * targetJumpHeight * (-1 * Physics2D.gravity.y));
+		rgbd = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
         anim.SetBool("running", false);
         anim.SetBool("facing_left", false);
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
         anim.SetBool("jumping", !grounded);
         if (Input.GetAxis("Horizontal") > 0) {
             anim.SetBool("facing_left", false);
@@ -28,23 +34,23 @@ public class PlayerMovement : MonoBehaviour {
         }
         if (grounded) {
             if (Input.GetButtonDown("Jump")) {
-                rgbd.AddForce(Vector2.up * jumpforce);
-                grounded = false;
+				Debug.Log("Jumping!");
+				rgbd.velocity += new Vector2(0f, jumpForce);
+				grounded = false;
             }
             if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0) {
                 anim.SetBool("running", true);
-            }
-            else
+            } else
                 anim.SetBool("running", false);
         }
         rgbd.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rgbd.velocity.y);
 	}
 
-    void OnCollisionEnter2D(Collision2D coll) {
-        if (coll.gameObject.tag == "Ground") {
-            if (coll.enabled)
-                grounded = true;
-        }
-
+    void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Ground") {
+			Debug.Log("Collide");
+			grounded = true;
+		}
     }
 }
