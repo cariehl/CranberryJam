d15f2df884cde0702906c24ee0db51efb573d22a
Tomas <tomamed@umich.edu>
2015-11-21 12:56:04 -0500

    Ropes WOrking

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -6,10 +6,11 @@ public class Rope : MonoBehaviour {
 	public Vector3 objective;
 	
 	public int num_ropes = 1;
-	private bool arrived;
+	public bool arrived = false;
 	private float temp;
 	private float trans;
 	public float radius;
+	public bool roping;
 
 	void Awake()
 	{
@@ -18,28 +19,31 @@ public class Rope : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		radius = 0.45f;//GetComponent<CircleCollider2D> ().radius;
+		//radius = 0.45f;//GetComponent<CircleCollider2D> ().radius;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (arrived) {
-			gameObject.GetComponent<Rigidbody2D> ().gravityScale = temp;
-		} 
-		else {
-			if (trans > 0.9f){
-				trans = 0f;
-				arrived = true;
-			}
-			else {
-				trans += 0.1f;
-				transform.position = Vector3.Lerp(transform.position, new Vector3(objective.x, objective.y, 0f), trans);
+		if (roping) {
+			if (arrived) {
+				gameObject.GetComponent<Rigidbody2D> ().gravityScale = temp;
+			} else {
+				if (trans > 0.9f) {
+					trans = 0f;
+					arrived = true;
+					roping = false;
+					gameObject.GetComponent<Rigidbody2D> ().gravityScale = temp;
+				} else {
+					trans += 0.1f;
+					transform.position = Vector3.Lerp (transform.position, new Vector3 (objective.x, objective.y, 0f), trans);
+				}
 			}
 		}
 	}
 
 	public void moveToRope()
 	{
+		roping = true;
 		arrived = false;
 		temp = gameObject.GetComponent<Rigidbody2D>().gravityScale;
 		gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
