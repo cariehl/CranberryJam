bc8ca7aee3f3284552947e410639d264fc0bc487
Tomas <tomamed@umich.edu>
2015-11-21 02:38:39 -0500

    Rope Start

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+	private bool hitch = false;
+	public Transform cr_objective ; 
+	void Update()
+	{
+		//position = new Rect((Screen.width - crosshair.width+Input.mousePosition.x) / 2, (Screen.height - crosshair.height - Input.mousePosition.y) /2, crosshair.width, crosshair.height);
+		transform.localPosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y) - new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight) / 2;
+		if (Input.GetButtonDown("Fire1"))
+		{
+			if (!hitch) Rope.S.objective = gameObject.transform;
+			else Rope.S.objective = cr_objective;
+		}
+
+	}
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.tag.Equals ("Hitch") ) {
+			hitch = true;
+			cr_objective = coll.transform;
+		}
+
+	}
+
+
+
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		hitch = false;
+	}
+}
