44922dae1ca63b294c60649e94bf6115981f1fb4
gatriex <haasjac@gmail.com>
2015-11-20 22:04:00 -0500

    Player Movement
    Simple player movement script. Also jake scene and scene folder name
    change

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 1;
+    private Rigidbody2D rgbd;
+
+	// Use this for initialization
+	void Start () {
+        rgbd = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rgbd.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+	}
+}
