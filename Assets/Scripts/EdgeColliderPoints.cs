dfcd484726efa6eb465796a9b010e1b4e061a886
Cooper Riehl <cariehl@umich.edu>
2015-11-21 04:34:39 -0500

    Moved some prefabs into the correct folder

---
+++ Assets/Scripts/EdgeColliderPoints.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeColliderPoints : MonoBehaviour
+{
+	public Vector2[] vertices;
+	EdgeCollider2D coll;
+
+	// Use this for initialization
+	void Start ()
+	{
+		coll = GetComponent<EdgeCollider2D>();
+		coll.points = vertices;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
