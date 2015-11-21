f9464306a2dead2441aa1e434a14db5122562135
Cooper Riehl <cariehl@umich.edu>
2015-11-21 05:30:55 -0500

    Cleaned up PlayerMovement script a little

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-
-    public float speed = 1;
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
 	// Target jump height in terms of units
     public float targetJumpHeight = 3.2f;
 	private float jumpForce;
@@ -18,22 +18,22 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
         if (grounded) {
             if (Input.GetButtonDown("Jump")) {
-                //rgbd.AddForce(Vector2.up * jumpForce);
-				rgbd.velocity += new Vector2(0.0f, jumpForce);
+				rgbd.velocity += new Vector2(0f, jumpForce);
                 grounded = false;
             }
         }
         rgbd.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rgbd.velocity.y);
 	}
 
-    void OnCollisionEnter2D(Collision2D coll) {
+    void OnCollisionEnter2D(Collision2D coll)
+	{
         if (coll.gameObject.tag == "Ground") {
             if (coll.enabled)
                 grounded = true;
         }
-
     }
 }
