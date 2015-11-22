7a7aca595974c31137889450cea9c1f5ec3236ba
gatriex <haasjac@gmail.com>
2015-11-22 10:25:13 -0500

    Door

---
+++ Assets/door.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerStay2D(Collider2D coll) {
+        if (Input.GetButtonDown("Fire1")) {
+            CavasFade.S.tent = true;
+            Application.LoadLevel(0);
+        }
+    }
+}
